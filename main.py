82b8950a96c0c59fa6c9a3473988e46607a8661f
Ostarius <ostarius.omnia@gmail.com>
2014-07-11 14:07:39 +0800

    Update main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+# QUICK NOTE: Cos 'for' loops are retarded in scripting languages, I've done it the lazy way and used 'while' loops instead.
+# I'll fix it later, I just wanted to get down the flow and structure etc first to make it semi-finalized before I share. 
+
+# ANOTHER QUICK NOTE: Read self.py and enviro.py first. It will make a lot more sense. 
